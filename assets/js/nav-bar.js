1f6a06038a54ac76e4fabdcc09e8a6f28ff4b25e
Jamie <jamie.styx@gmail.com>
2023-07-22 23:40:05 +0900

    added support for tablets

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -8,9 +8,9 @@ class NavBar extends HTMLElement {
     <nav>
       <div class="nav-h1"><a href="index.html"><h1>Jamie Douglas</h1></a></div>
         <ul class="navigation">
-        <li><a href="#about" class="nav-link">About</a></li>
+        <li><a href="index.html" class="nav-link">About</a></li>
         <li><a href="#skills" class="nav-link">Skills</a></li>
-        <li><a href="#projects" class="nav-link">Projects</a></li>
+        <li><a href="projects.html" class="nav-link">Projects</a></li>
         <li><a href="contact.html" class="nav-link">Contact</a></li>
       </ul>
       <button class="burger-menu" id="burger-menu">
@@ -22,3 +22,20 @@ class NavBar extends HTMLElement {
 }
 
 customElements.define('navigation-component', NavBar);
+
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const navLink = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+
