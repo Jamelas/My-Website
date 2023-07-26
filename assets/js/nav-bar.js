28a07272b2e688e5bdcae1c055304c3daf381561
Jamie <jamie.styx@gmail.com>
2023-07-26 21:50:02 +0900

    added div to fill remaining space

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -5,18 +5,20 @@ class NavBar extends HTMLElement {
   
   connectedCallback() {
     this.innerHTML = `
-    <nav>
-      <div class="nav-h1"><a href="index.html"><h1>Jamie Douglas</h1></a></div>
-        <ul class="navigation">
-        <li><a href="index.html" class="nav-link">About</a></li>
-        <li><a href="skills.html" class="nav-link">Skills</a></li>
-        <li><a href="projects.html" class="nav-link">Projects</a></li>
-        <li><a href="contact.html" class="nav-link">Contact</a></li>
-      </ul>
-      <button class="burger-menu" id="burger-menu">
-        <ion-icon class="bars" name="menu-outline"></ion-icon>
-      </button>
-    </nav>
+    <div class="nav-behind">
+      <nav>
+        <div class="nav-h1"><a href="index.html"><h1>Jamie Douglas</h1></a></div>
+          <ul class="navigation">
+          <li><a href="index.html" class="nav-link">About</a></li>
+          <li><a href="skills.html" class="nav-link">Skills</a></li>
+          <li><a href="projects.html" class="nav-link">Projects</a></li>
+          <li><a href="contact.html" class="nav-link">Contact</a></li>
+        </ul>
+        <button class="burger-menu" id="burger-menu">
+          <ion-icon class="bars" name="menu-outline"></ion-icon>
+        </button>
+      </nav>
+    </div>
     `;
   }
 }
